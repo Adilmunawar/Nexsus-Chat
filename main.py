c73f8dfcba7cdfaed092ed6de99461de083420e2
Adil Munawar <127094634+Adilmunawar@users.noreply.github.com>
2025-07-18 13:32:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-try:
-	with open("status.md" , "r") as f:
-		text = f.readline()
-		num = [int(x) for x in text.split() if x.isdigit()][0]
-		num += 1
-	
-	if num<= 1024:
-		with open("status.md" , 'w') as f:
-			if num in range(16):
-				
+
