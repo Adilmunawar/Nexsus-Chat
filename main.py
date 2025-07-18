93560b38458eee62d81ef9b6415454f2e0bf091c
Adil Munawar <127094634+Adilmunawar@users.noreply.github.com>
2025-07-18 13:32:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,4 @@ try:
 	if num<= 1024:
 		with open("status.md" , 'w') as f:
 			if num in range(16):
-				img = "![pull-shark](images/pull-shark-default.png)"
-			elif num in range(128):
-				img = "![pull-shark](images/pull-shark-bronze.png)"
-			elif num in range(1024):
-				img = "![pull-shark](images/pull-shark-silver.png)"
-			else:
-				img = "![pull-shark](images/pull-shark-gold.png)"
-			
-			f.write(f"{num} pull requests merged")
-			f.write("<br>")
-			f.write("Currently:")
-			f.write("<br>")
-			f.write(img)
-except:
-	with open("status.md" , "w") as f:
-		f.write("1 pull request merged")
+				
