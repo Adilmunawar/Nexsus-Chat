8c2eb401af1cbc5259b3d81327c23ee900806260
Adil Munawar <127094634+Adilmunawar@users.noreply.github.com>
2025-07-23 13:42:04 +0500

    Update Avatar.tsx

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,17 +1,13 @@
 'use client';
-
 import { User } from '@prisma/client';
 import Image from 'next/image';
 import useActiveList from '../hooks/useActiveList';
-
 interface AvatarProps {
   user: User;
 }
-
 const Avatar: React.FC<AvatarProps> = ({ user }) => {
   const { members } = useActiveList();
   const isActive = members.indexOf(user?.email) !== -1;
-
   return (
     <div className="relative">
       <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
