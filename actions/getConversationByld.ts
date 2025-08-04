c71876e2f14d80b40ea94c56eeee0d96afd4e162
Adil Munawar <adilfromavengers@gmail.com>
2025-08-04 16:59:48 +0500

    Update getConversationByld.ts

--- actions/getConversationByld.ts
+++ actions/getConversationByld.ts
@@ -1,10 +1,8 @@
 import { IconType } from 'react-icons';
-
 interface AuthSocialButtonProps {
   icon: IconType;
   onClick: () => void;
 }
-
 const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
   icon: Icon,
   onClick,
